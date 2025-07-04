3ec1ee6330d654a7e8bec33c610c13f207125e0f
Bangdx1412 <doxuanbang14122005@gmail.com>
2025-07-04 12:13:09 +0700

    Sử dụng destructering để phá vỡ cấu trúc

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,9 +1,10 @@
 function Content(props){
+      const {text, so, active} = props;
     // console.log(props);
     return (
         <>
-            <div className={"box " + (props.active? "box--active" : "")}>
-                {props.text} - Số lượng: {props.so ? props.so : ""} 
+            <div className={"box " + (active? "box--active" : "")}>
+                {text} - Số lượng: {so} 
             </div>
         </>
     )
