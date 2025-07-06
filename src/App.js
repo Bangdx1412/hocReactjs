627fd1b11bbd491b8e20c582fc12ba7a7cb45631
Bangdx1412 <doxuanbang14122005@gmail.com>
2025-07-06 19:57:54 +0700

    Học về useEffect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import "./App.css";
-import Modal from "./components/Modal";
-// import Cart from "./components/Cart";
-// import Country from "./components/Country";
-// import Lamp from "./components/Lamp";
-// import Login from "./components/login";
-// import Menu from "./components/Menu";
-// import ProductList from "./components/ProductList";
+
 
 function App() {
   return (
     <>
-      {/* <Login/>
-      <Menu/> */}
-      {/* <ProductList/> */}
-      {/* <Country/> */}
-      {/* <Lamp/> */}
-      {/* <Cart/> */}
-      <Modal/>
+ 
     </>
   );
 }
--- src/components/Menu/index.js
+++
@@ -1,9 +0,0 @@
-function Menu() {
-  const arrayMenu = ["Trang chủ", "Sản phẩm", "Tin Tức", "Liên hệ"];
-  return (
-    <>
-      {arrayMenu.map((item, index) => (<li key={index}>{item}</li>))}
-    </>
-  );
-}
-export default Menu;
